0990163d0ecf5a715193793e7b061867a45e7393
Prabh Sagoo <prabhsagoo@yahoo.com>
2023-05-31 10:36:14 -0600

    run build

--- backend/server.js
+++ backend/server.js
@@ -1,21 +1,30 @@
 import express, { json } from "express";
 import { getAllCommunities } from "./Firebase/dataGrab.js";
 import { userRouter } from "./Routes/user.js";
-
+import path from "path";
 
 const app = express();
+
+const __dirname = path.resolve();
+const pathToBuild = path.join(__dirname, "../frontend/dist");
+console.log(pathToBuild);
+app.use(express.static(pathToBuild));
+
 const PORT = 5150;
 app.use(express.json());
 app.use("/api/user", userRouter);
 
 app.get("/api", async (request, response) => {
-    let a =  (await getAllCommunities());
-    // let a = await (await getAllCommunities()).map((a)=>a.communityAssociationName);
-    // console.log(a);
-    response.send(a);
-  });
+  let a = await getAllCommunities();
+  // let a = await (await getAllCommunities()).map((a)=>a.communityAssociationName);
+  // console.log(a);
+  response.send(a);
+});
 
-  app.listen(PORT, function () {
-    console.log(`Listening on port ${PORT}`);
-  });
+app.use("*", (req, res) => {
+  res.sendFile(path.join(pathToBuild, "index.html"));
+});
 
+app.listen(PORT, function () {
+  console.log(`Listening on port ${PORT}`);
+});
